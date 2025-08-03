a13f6469c7858fd48fc5cd1319d5cd38199c14c7
ChanduThota007 <chanduthota0209@gmail.com>
2025-08-03 11:08:46 +0530

    Clean and fix manifest path

---
+++ app/src/main/java/dev/refillr/kitchenbuddy/DelegationService.java
@@ -0,0 +1,14 @@
+package dev.refillr.kitchenbuddy;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
